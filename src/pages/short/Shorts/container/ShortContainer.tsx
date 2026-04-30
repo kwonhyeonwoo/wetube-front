a33ab64e50cd173c84668e547b3e0d181a129eb8
keonhyeonwoo <khw19980926@gmail.com>
2026-04-30 14:12:05 +0900

    refactor: 컴포넌트명 변경, 경로 수정

---
+++ src/pages/short/Shorts/container/ShortContainer.tsx
@@ -0,0 +1,6 @@
+import styles from "../css/index.module.css";
+const ShortContainer = () => {
+  return <main className={styles.shortPage}>ShortContainer</main>;
+};
+
+export default ShortContainer;
